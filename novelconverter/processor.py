5fc90837043b1149b0714c8b3d6d6e5246f4b6d1
Ray Shirohara <47940764+RShirohara@users.noreply.github.com>
2020-09-19 17:01:49 +0900

    Add class "PreProcessor" and "PostProcessor"

--- novelconverter/processor.py
+++ novelconverter/processor.py
@@ -1,2 +1,29 @@
 # -*- coding: utf-8 -*-
 # author: RShirohara
+
+
+from . import util
+
+
+def build_preprocessor():
+    """Build the default preprocessors."""
+    processor = PreProcessor()
+    # processor.reg.add(function, name, priority)
+    return processor
+
+
+def build_postprocessor():
+    """Build the default postprocessors."""
+    processor = PostProcessor()
+    # processor.reg.add(function, name, priority)
+    return processor
+
+
+class PreProcessor(util.Processor):
+    """Process a strings before running parser"""
+    pass
+
+
+class PostProcessor(util.Processor):
+    """Process a strings after running renderer"""
+    pass
