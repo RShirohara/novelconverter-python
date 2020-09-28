b45ffa78b6c17ee2c844ee48cc9ace50c1cca138
Ray Shirohara <47940764+RShirohara@users.noreply.github.com>
2020-09-28 14:35:27 +0900

    Add function "build_*" in Pixiv

--- novelconverter/extension/pixiv.py
+++ novelconverter/extension/pixiv.py
@@ -7,22 +7,42 @@ import re
 from novelconverter import parser, renderer
 
 _IMAGE = re.compile(r"!\[pixivimage:(?P<link>.*?)")
-_LINK = re.compile(r"\[jumpurl:(?P<string>.*?)>(?P<url>.*?)\]")
+_LINK = re.compile(r"\[jumpurl:(?P<text>.*?)>(?P<link>.*?)\]")
 _RUBY = re.compile(r"\[\[rb:(?P<text>.*?)>(?P<ruby>.*?)\]\]")
 _HEADER = re.compile(r"\[chapter:(?P<name>.*?)\]")
 _NEW_PAGE = re.compile(r"\[newpage\]")
 
 
 def build_inlineparser():
-    pass
+    parser = InlineParser()
+    parser.reg.add(parser.image, "image", 30)
+    parser.reg.add(parser.link, "link", 20)
+    parser.reg.add(parser.ruby, "ruby", 10)
+    return parser
 
 
 def build_blockparser():
-    pass
+    parser = BlockParser()
+    parser.reg.add(parser.newpage, "newpage", 30)
+    parser.reg.add(parser.header, "header", 20)
+    parser.reg.add(parser.para, "para", 10)
+    return parser
 
 
 def build_renderer():
-    pass
+    renderer = Renderer()
+    renderer.reg.add(renderer.bold, "bold", 110)
+    renderer.reg.add(renderer.image, "image", 100)
+    renderer.reg.add(renderer.link, "link", 90)
+    renderer.reg.add(renderer.ruby, "ruby", 80)
+    renderer.reg.add(renderer.tcy, "tcy", 70)
+    renderer.reg.add(renderer.newpage, "newpage", 60)
+    renderer.reg.add(renderer.header, "header", 50)
+    renderer.reg.add(renderer.code_block, "code_block", 40)
+    renderer.reg.add(renderer.item_list, "item_list", 30)
+    renderer.reg.add(renderer.quote, "quote", 20)
+    renderer.reg.add(renderer.para, "para", 10)
+    return renderer
 
 
 class InlineParser(parser.InlineParser):
@@ -51,7 +71,7 @@ class InlineParser(parser.InlineParser):
             _dict = _match.groupdict()
             _old = _match.group(0)
             _new = "{\"type\": \"link\", " + \
-                f"content\": [\"{_dict['string']}\", \"{_dict['url']}\"]" + \
+                f"content\": [\"{_dict['text']}\", \"{_dict['link']}\"]" + \
                 "}"
             source = source.replace(_old, _new)
         return source
