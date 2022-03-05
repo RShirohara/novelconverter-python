6b372e65b29990287d43bcd9ad935a6c9fed0853
Ray Shirohara <47940764+RShirohara@users.noreply.github.com>
2022-03-05 18:12:27 +0900

    feat: change returns of Registry
    Add key and priority to returns of Registry.
    Before, registry returned only item.

--- novelconverter/util.py
+++ novelconverter/util.py
@@ -1,26 +1,102 @@
 # -*- coding: utf-8 -*-
 # author: @RShirohara
+
 """NovelConverter Utility module.
 
 Attributes:
     Registry: A list sorted by priority.
 """
 
-from typing import Iterator, overload
+from dataclasses import dataclass, field
+from typing import Container, Iterable, Iterator, overload
 
 
-class Registry:
-    """A list sorted by priority.
+@dataclass(repr=False)
+class Registry(Container):
+    """A registry sorted by priority.
 
     Registry is a list-like data structure.
-    Registry is sorted of priority (ascending order).
+    Registry item is sorted of prioritry (ascending order).
     """
 
-    def __init__(self) -> None:
-        self.__data: dict[str, any] = {}
-        self.__priority: dict[str, int] = {}
-        self.__key_cache: list[str] = []
-        self.__is_sorted: bool = False
+    __data: dict[str, any] = field(default_factory=dict[str, any], init=False)
+    __priority: dict[str, int] = field(
+        default_factory=dict[str, int], init=False
+    )
+    __key_cache: tuple[str] = field(
+        default_factory=tuple[str], init=False, hash=False, compare=False
+    )
+    __is_sorted: bool = field(
+        default=False, init=False, hash=False, compare=False
+    )
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}" + tuple(self.__iter__()).__repr__()
+
+    def __len__(self) -> int:
+        return len(self.__priority)
+
+    @overload
+    def __getitem__(self, key: int) -> tuple[str, int, any]:
+        pass
+
+    @overload
+    def __getitem__(self, key: str) -> tuple[int, any]:
+        pass
+
+    @overload
+    def __getitem__(self, key: slice) -> "Registry[any]":
+        pass
+
+    def __getitem__(self, key: int | str | slice) -> any:
+        self.__sort()
+        match key:
+            case int() if type(key) != bool:
+                k: str = self.__key_cache[key]
+                return (k, self.__priority[k], self.__data[k])
+            case str():
+                if key not in self.__priority.keys():
+                    raise KeyError(key)
+                return (self.__priority[key], self.__data[key])
+            case slice():
+                items: tuple[tuple[str, int, any]] = tuple(
+                    (k, self.__priority[k], self.__data[k])
+                    for k in self.__key_cache[key]
+                )
+                reg: Container[any] = Registry()
+                reg.add_items(items)
+                return reg
+            case _:
+                raise TypeError(
+                    "registry indices must be integers, strings or slices"
+                )
+
+    def __delitem__(self, key: int | str) -> any:
+        target: str
+
+        self.__sort()
+        match key:
+            case int() if type(key) != bool:
+                target = self.__key_cache[key]
+            case str():
+                target = key
+            case _:
+                raise TypeError(
+                    "registry indices on delete must be integers or strings"
+                )
+        if target not in self.__priority.keys():
+            raise KeyError(target)
+
+        self.__is_sorted = False
+        del self.__priority[target]
+        del self.__data[target]
+
+    def __iter__(self) -> Iterator[tuple[str, int, any]]:
+        self.__sort()
+        return iter(
+            (key, self.__priority[key], self.__data[key])
+            for key in self.__key_cache
+        )
 
     def __contains__(self, item: any) -> bool:
         self.__sort()
@@ -33,81 +109,37 @@ class Registry:
             case _:
                 return item in self.__data.values()
 
-    def __delitem__(self, key: int | str) -> None:
-        self.__sort()
-        target: str = ""
-        match key:
-            case int() if type(key) != bool:
-                target = self.__key_cache[key]
-            case str():
-                target = key
-            case _:
-                raise TypeError(
-                    "registry indices on delete must be integers or strings"
-                )
-        if target not in self.__priority.keys():
-            raise KeyError(target)
-        self.__is_sorted = False
-        del self.__priority[target]
-        del self.__data[target]
-
-    @overload
-    def __getitem__(self, key: int) -> any:
-        pass
-
-    @overload
-    def __getitem__(self, key: str) -> any:
-        pass
-
-    @overload
-    def __getitem__(self, key: slice) -> "Registry[any]":
-        pass
-
-    def __getitem__(self, key: int | str | slice) -> any:
-        self.__sort()
-        match key:
-            case int() if type(key) != bool:
-                return self.__data[self.__key_cache[key]]
-            case str():
-                if key not in self.__priority.keys():
-                    raise KeyError(key)
-                return self.__data[key]
-            case slice():
-                reg = Registry()
-                for k in self.__key_cache[key]:
-                    reg.add(k, self.__priority[k], self.__data[k])
-                return reg
-            case _:
-                raise TypeError(
-                    "registry indices must be integers, strings or slices"
-                )
-
-    def __iter__(self) -> Iterator[any]:
-        self.__sort()
-        return iter(self.__data[key] for key in self.__key_cache)
-
-    def __len__(self) -> int:
-        return len(self.__priority)
-
     def __sort(self) -> None:
         if not self.__is_sorted:
             sorted_priority: list[tuple[str, int]] = sorted(
                 self.__priority.items(), key=lambda x: x[1]
             )
-            self.__key_cache = [key for key, _ in sorted_priority]
+            self.__key_cache = tuple(key for key, _ in sorted_priority)
             self.__is_sorted = True
 
     def add(self, key: str, priority: int, item: any) -> None:
-        """Add an item to the registry with the given name and priority.
+        """Add an item to the registry.
 
         Args:
-            key (str): key of item.
-            priority (int): priority of item.
-            item (any): item.
+            key (str): Key used to reference item.
+            priority (int): Priority used for sort in registry.
+            item (any): Item added to registry.
         """
 
+        # Block duplicates with existing items.
         if key in self.__priority:
             self.__delitem__(key)
+
         self.__is_sorted = False
         self.__data[key] = item
         self.__priority[key] = priority
+
+    def add_items(self, items: Iterable[tuple[str, int, any]]) -> None:
+        """Add items to the registry all at once.
+
+        Args:
+            items (Iterable[tuple[str, int, any]]): Items added to registry.
+        """
+
+        for key, priority, item in items:
+            self.add(key, priority, item)
--- tests/test_util.py
+++ tests/test_util.py
@@ -11,34 +11,48 @@ from novelconverter.util import Registry
 from pytest import fixture, raises
 
 
+datas: tuple[tuple[str, int, any]] = (
+    ("foo", 10, "Test string."),
+    ("bar", 30, 123),
+    ("baz", 20, True),
+)
+
+
 @fixture
 def registry() -> Registry:
-    reg = Registry()
-    reg.add("foo", 10, "Test string.")
-    reg.add("bar", 30, 123)
-    reg.add("baz", 20, True)
+    reg: Registry[any] = Registry()
+    reg.add_items(datas)
     yield reg
 
 
 class TestRegistry:
-    def test_contains(self, registry: Registry) -> None:
-        # if registry contains key, return True.
-        assert "foo" in registry
-        assert "bar" in registry
-        assert "baz" in registry
+    def test_getitem(self, registry: Registry) -> None:
+        # if access using an existing key, return values.
+        assert datas[0][1:] == registry["foo"]
+        assert datas[1][1:] == registry["bar"]
+        assert datas[2][1:] == registry["baz"]
 
-        # if registry contains value, return True.
-        assert "Test string." in registry
-        assert 123 in registry
-        assert True in registry
+        # if access using an index in range, return values.
+        assert datas[0] == registry[0]
+        assert datas[2] == registry[1]
+        assert datas[1] == registry[2]
 
-        # if registry not contains key, return False.
-        assert "hoge" not in registry
+        # if access using an slice, return registry.
+        assert datas[0] == registry[0:2][0]
+        assert datas[2] == registry[0:2][1]
+        assert datas[1] == registry[0:3:2][1]
 
-        # if registry not contains value, return False.
-        assert "This is test string" not in registry
-        assert 456 not in registry
-        assert False not in registry
+        # if access using an non-existing key, raise KeyError.
+        with raises(KeyError):
+            registry["hoge"]
+
+        # if access using index out of range, raise IndexError.
+        with raises(IndexError):
+            registry[123]
+
+        # if access using other-type values, raise TypeError.
+        with raises(TypeError):
+            registry[("foo", "Bar")]
 
     def test_delitem(self, registry: Registry) -> None:
         # if try to delete item by key, it will success.
@@ -61,46 +75,47 @@ class TestRegistry:
         with raises(IndexError):
             del registry[123]
 
-    def test_getitem(self, registry: Registry) -> None:
-        # if access using an existing key, return values.
-        assert "Test string." == registry["foo"]
-        assert 123 == registry["bar"]
-        assert True is registry["baz"]
-
-        # if access using an index in range, return values.
-        assert "Test string." == registry[0]
-        assert True is registry[1]
-        assert 123 == registry[2]
-
-        # if access using an slice, return registry.
-        assert "Test string." == registry[0:2][0]
-        assert True is registry[0:2][1]
-        assert 123 == registry[0:3:2][1]
-
-        # if access using an non-existing key, raise KeyError.
-        with raises(KeyError):
-            registry["hoge"]
-
-        # if access using index out of range, raise IndexError.
-        with raises(IndexError):
-            registry[123]
-
-        # if access using other-type values, raise TypeError.
-        with raises(TypeError):
-            registry[("foo", "Bar")]
-
     def test_iter(self, registry: Registry) -> None:
-        assert ("Test string.", True, 123) == tuple(x for x in registry)
+        assert tuple(sorted(datas, key=lambda x: x[1])) == tuple(
+            x for x in registry
+        )
+
+    def test_contains(self, registry: Registry) -> None:
+        # if registry contains key, return True.
+        assert "foo" in registry
+        assert "bar" in registry
+        assert "baz" in registry
+
+        # if registry contains value, return True.
+        assert "Test string." in registry
+        assert 123 in registry
+        assert True in registry
+
+        # if registry not contains key, return False.
+        assert "hoge" not in registry
+
+        # if registry not contains value, return False.
+        assert "This is test string" not in registry
+        assert 456 not in registry
+        assert False not in registry
 
     def test_add(self, registry: Registry) -> None:
-        registry.add("hoge", 15, [1, 2, 3])
-        assert "hoge" in registry
-        assert [1, 2, 3] in registry
-        assert [1, 2, 3] == registry["hoge"]
-        assert [1, 2, 3] == registry[1]
+        add_data: tuple[tuple[str, int, any]] = (
+            ("hoge", 15, [1, 2, 3]),
+            ("bar", 40, ("Test", 123)),
+        )
 
-        registry.add("bar", 40, ("Test", 123))
-        assert "bar" in registry
-        assert ("Test", 123) in registry
-        assert ("Test", 123) == registry["bar"]
-        assert ("Test", 123) == registry[3]
+        registry.add(add_data[0][0], add_data[0][1], add_data[0][2])
+        assert add_data[0][0] in registry
+        assert add_data[0][2] in registry
+        assert add_data[0][1:] == registry["hoge"]
+        assert add_data[0] == registry[1]
+
+        registry.add(add_data[1][0], add_data[1][1], add_data[1][2])
+        assert add_data[1][0] in registry
+        assert add_data[1][2] in registry
+        assert add_data[1][1:] == registry["bar"]
+        assert add_data[1] == registry[3]
+
+    def test_add_items(self, registry: Registry) -> None:
+        pass
