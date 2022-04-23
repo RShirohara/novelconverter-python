5973bc5d039d00e0c0576e65ef989379ed53b230
Ray Shirohara <47940764+RShirohara@users.noreply.github.com>
2022-04-23 22:01:28 +0900

    test: refactor registry
    Refactor Registry tests.

--- tests/test_util.py
+++ tests/test_util.py
@@ -7,10 +7,10 @@ This test will be run using pytest.
 """
 
 
-from typing import Any
+from typing import Any, Generator
+from pytest import fixture, raises
 
 from novelconverter.util import Registry, RegistryItem
-from pytest import fixture, raises
 
 datas: tuple[RegistryItem, ...] = (
     RegistryItem("foo", 10, "Test string."),
@@ -20,14 +20,101 @@ datas: tuple[RegistryItem, ...] = (
 
 
 @fixture
-def registry():
+def registry() -> Generator[Registry[Any], None, None]:
+    """Build test data.
+
+    Yields:
+        Registry[Any]: Test data.
+    """
+
     reg: Registry[Any] = Registry()
     reg.add_items(datas)
     yield reg
 
 
 class TestRegistry:
-    def test_getitem(self, registry: Registry) -> None:
+    """Test class of Registry."""
+
+    def test_additem(self, registry: Registry):
+        """Test Registry.__additem__
+
+        Args:
+            registry (Registry): Test data.
+        """
+
+        # if add item by non-exist key, it will success.
+        registry.add("hoge", 15, [1, 2, 3])
+        assert "hoge" in registry
+        assert [1, 2, 3] in registry
+        assert [1, 2, 3] == registry["hoge"]
+        assert [1, 2, 3] == registry[1]
+
+        # if add item by exist key, Overwrite to value.
+        registry.add("bar", 40, ("Test", 123))
+        assert "bar" in registry
+        assert ("Test", 123) in registry
+        assert ("Test", 123) == registry["bar"]
+        assert ("Test", 123) == registry[3]
+
+    def test_contains(self, registry: Registry) -> None:
+        """Test Registry.__contains__.
+
+        Args:
+            registry (Registry): Test data.
+        """
+
+        # if registry contains key, return True.
+        assert "foo" in registry
+        assert "bar" in registry
+        assert "baz" in registry
+
+        # if registry contains value, return True.
+        assert "Test string." in registry
+        assert 123 in registry
+        assert True in registry
+
+        # if registry not contains key, return False.
+        assert "hoge" not in registry
+
+        # if registry not contains value, return False.
+        assert "This is test string" not in registry
+        assert 456 not in registry
+        assert False not in registry
+
+    def test_delitem(self, registry: Registry) -> None:
+        """Test Registry.__delitem__
+
+        Args:
+            registry (Registry): Test data.
+        """
+
+        # if try to delete item by key, it will success.
+        del registry["foo"]
+        assert "Test string." not in registry
+
+        # if try to delete item by index, it will success.
+        del registry[1]
+        assert 456 not in registry
+
+        # if try to delete item by non-exist key, it will fail.
+        with raises(KeyError):
+            del registry["hoge"]
+
+        # if try to delete item by value, it will fail.
+        with raises(TypeError):
+            del registry[True]
+
+        # if try to delete item by index out of range, it will fail.
+        with raises(IndexError):
+            del registry[123]
+
+    def test_getitem(self, registry: Registry):
+        """Test Registry.__getitem__.
+
+        Args:
+            registry (Registry): Test data.
+        """
+
         # if access using an existing key, return values.
         assert "Test string." == registry["foo"]
         assert 123 == registry["bar"]
@@ -55,95 +142,77 @@ class TestRegistry:
         with raises(TypeError):
             registry[("foo", "Bar")]
 
-    def test_delitem(self, registry: Registry) -> None:
-        # if try to delete item by key, it will success.
-        del registry["foo"]
-        assert "Test string." not in registry
+    def test_iter(self, registry: Registry):
+        """Test Registry.__iter__
 
-        # if try to delete item by index, it will success.
-        del registry[1]
-        assert 456 not in registry
+        Args:
+            registry (Registry): Test data.
+        """
 
-        # if try to delete item by non-exist key, it will fail.
-        with raises(KeyError):
-            del registry["hoge"]
+        assert tuple(sorted(datas, key=lambda x: x[1])) == tuple(x for x in registry)
 
-        # if try to delete item by value, it will fail.
-        with raises(TypeError):
-            del registry[True]
+    def test_len(self, registry: Registry):
+        """Test Registry.__len__
 
-        # if try to delete item by index out of range, it will fail.
-        with raises(IndexError):
-            del registry[123]
+        Args:
+            registry (Registry): Test data.
+        """
 
-    def test_iter(self, registry: Registry) -> None:
-        assert tuple(sorted(datas, key=lambda x: x[1])) == tuple(
-            x for x in registry
+        assert 3 == len(registry)
+
+    def test_repr(self, registry: Registry):
+        """Test Registry.__repr__
+
+        Args:
+            registry (Registry): Test data.
+        """
+
+        assert (
+            "Registry(\
+RegistryItem(key='foo', priority=10, item='Test string.'), \
+RegistryItem(key='baz', priority=20, item=True), \
+RegistryItem(key='bar', priority=30, item=123))"
+            == repr(registry)
         )
 
-    def test_contains(self, registry: Registry) -> None:
-        # if registry contains key, return True.
-        assert "foo" in registry
-        assert "bar" in registry
-        assert "baz" in registry
+    def test_keys(self, registry: Registry):
+        """Test Registry.keys
 
-        # if registry contains value, return True.
-        assert "Test string." in registry
-        assert 123 in registry
-        assert True in registry
+        Args:
+            registry (Registry): Test data.
+        """
 
-        # if registry not contains key, return False.
-        assert "hoge" not in registry
+        assert ("foo", "bar", "baz") == registry.keys()
 
-        # if registry not contains value, return False.
-        assert "This is test string" not in registry
-        assert 456 not in registry
-        assert False not in registry
+    def test_pop(self, registry: Registry):
+        """Test Registry.pop
 
-    def test_add(self, registry: Registry) -> None:
-        add_data: tuple[RegistryItem, ...] = (
-            RegistryItem("hoge", 15, [1, 2, 3]),
-            RegistryItem("bar", 40, ("Test", 123)),
-        )
+        Args:
+            registry (Registry): Test data.
+        """
 
-        registry.add(add_data[0].key, add_data[0].priority, add_data[0].item)
-        assert "hoge" in registry
-        assert [1, 2, 3] in registry
-        assert [1, 2, 3] == registry["hoge"]
-        assert [1, 2, 3] == registry[1]
-
-        registry.add(add_data[1].key, add_data[1].priority, add_data[1].item)
-        assert "bar" in registry
-        assert ("Test", 123) in registry
-        assert ("Test", 123) == registry["bar"]
-        assert ("Test", 123) == registry[3]
-
-    def test_add_items(self, registry: Registry) -> None:
-        add_data: tuple[RegistryItem, ...] = (
-            RegistryItem("hoge", 15, [1, 2, 3]),
-            RegistryItem("bar", 40, ("Test", 123)),
-        )
-
-        registry.add_items(add_data)
-
-        assert "hoge" in registry
-        assert [1, 2, 3] in registry
-        assert [1, 2, 3] == registry["hoge"]
-        assert [1, 2, 3] == registry[1]
-
-        assert "bar" in registry
-        assert ("Test", 123) in registry
-        assert ("Test", 123) == registry["bar"]
-        assert ("Test", 123) == registry[3]
-
-    def test_pop(self, registry: Registry) -> None:
-        result: tuple[RegistryItem, ...] = (
-            RegistryItem("foo", 10, "Test string."),
-            RegistryItem("bar", 30, 123),
-        )
-
+        # if try delete item by key, return deleted value.
         assert True is registry.pop("baz")
-        assert result == tuple(r for r in registry)
+        assert True not in registry
 
+        # if try delete item by non-exist key, raise KeyError.
         with raises(KeyError):
             registry.pop("bazFix")
+
+    def test_priorities(self, registry: Registry):
+        """Test Registry.priorities.
+
+        Args:
+            registry (Registry): Test data.
+        """
+
+        assert {"foo": 10, "bar": 30, "baz": 20} == registry.priorities()
+
+    def test_values(self, registry: Registry):
+        """Test Registry.values.
+
+        Args:
+            registry (Registry): Test data.
+        """
+
+        assert ("Test string.", True, 123) == registry.values()
